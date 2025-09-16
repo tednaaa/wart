e5718bc974b5de3502c3007c4a41a1ddc9ba52bb
Andranik <tednaaa@gmail.com>
2025-09-16 11:09:05 +0400

    basic root page

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './views/router';
-// import './assets/css/main.css';
+import './assets/css/main.css';
 
 const app = createApp(App);
 
+app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { Button } from '@/shared/ui/button';
+</script>
+
 <template>
-  <h1 class="text-2xl font-medium flex items-center justify-center h-screen">Hello Kubernetes ;)</h1>
+  <div class="h-screen w-screen" :class="$style.root">
+    <div class="max-w-lg mx-auto pt-30">
+      <h2 class="bg-orange-500 px-2 py-1 rounded-lg mb-4">что написано в сурре 4 аяте 24?</h2>
+      <div class="grid grid-cols-2 gap-2">
+        <Button>можно убивать людей</Button>
+        <Button>можно насиловать женщин</Button>
+        <Button>можно насиловать мужщин</Button>
+        <Button>можно насиловать раба</Button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style module>
+.root {
+	background: url('@/views/assets/bg.png') no-repeat center center fixed;
+	background-size: cover;
+}
+</style>
